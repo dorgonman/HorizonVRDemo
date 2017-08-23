b996af8f1e4e79b0c39d7ae4a73e349e88c7e1ee
dorgon.chang <dorgonman@hotmail.com>
2017-08-23 23:40:58 +0800

    Initial commit

---
+++ Source/HorizonVRDemo.Target.cs
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class HorizonVRDemoTarget : TargetRules
+{
+	public HorizonVRDemoTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+
+		ExtraModuleNames.AddRange( new string[] { "HorizonVRDemo" } );
+	}
+}
---
+++ Source/HorizonVRDemo/HorizonVRDemoGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HorizonVRDemoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HORIZONVRDEMO_API AHorizonVRDemoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
