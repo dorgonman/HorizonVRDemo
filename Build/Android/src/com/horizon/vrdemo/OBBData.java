5bffe69cfb152432ea11822ddf74845edd98915d
dorgon.chang <dorgonman@hotmail.com>
2017-09-24 19:03:49 +0800

    update

---
+++ Build/Android/src/com/horizon/vrdemo/OBBData.java
@@ -0,0 +1,24 @@
+package com.horizon.vrdemo;
+
+public class OBBData
+{
+public static class XAPKFile {
+public final boolean mIsMain;
+public final String mFileVersion;
+public final long mFileSize;
+XAPKFile(boolean isMain, String fileVersion, long fileSize) {
+mIsMain = isMain;
+mFileVersion = fileVersion;
+mFileSize = fileSize;
+}
+}
+
+public static final XAPKFile[] xAPKS = {
+new XAPKFile(
+true, // true signifies a main file
+"15", // the version of the APK that the file was uploaded against
+43734310L // the length of the file in bytes
+)
+};
+};
+
